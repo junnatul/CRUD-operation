b60a576f3bc73273f26f308e427980026d19016e
Junnatul Nayem <junnatul.nayem@talview.com>
2022-05-18 19:25:15 +0530

    add delete functionality

--- script.js
+++ script.js
@@ -1,20 +1,64 @@
-let tbody = document.getElementById('table_body');
-fetch('./db.json')
-  .then(response => response.json())
-  .then(data => {
-    data.employeeData.map((item) => {
-      let newRow = `<tr>
-                      <td>${item.name}</td>
-                      <td>${item.employeeID}</td>
-                      <td>${item.designation}</td>
-                      <td>${item.city}</td>
-                      <td>${item.contact}</td>
-                      <td><button type="button">Edit</button>&nbsp<button type="button">Delete</button></td>
-                  </tr>`;
-      document.getElementById('table_body').innerHTML += newRow;
+// let tbody = document.getElementById('table_body');
+// let global = []
+// function render(){
+//   fetch('./db.json')
+// .then(response => response.json())
+// .then(data => {
+//   global = data.employeeData
+//   global.map((item,index) => {
+//     let newRow = `<tr>
+//                     <td>${item.name}</td>
+//                     <td>${item.employeeID}</td>
+//                     <td>${item.designation}</td>
+//                     <td>${item.city}</td>
+//                     <td>${item.contact}</td>
+//                     <td><button type="button">Edit</button>&nbsp<button onClick=deleteData(${index}) type="button">Delete</button></td>
+//                 </tr>`;
+//     document.getElementById('table_body').innerHTML += newRow;
+//   })
+// })
+// }
+// render()
+
+//   function deleteData(id){
+//     console.log(id)
+//     global.splice(id, 1);
+//   }
+let tbody = document.getElementById("table_body");
+let global = [];
+function getDataFromJson() {
+    fetch('./db.json')
+        .then(response => response.json())
+        .then(data => {
+          console.log(data)
+            global = data.employeeData;
+render();
+
+        })
+
+}
+getDataFromJson();
+
+function render() {
+  document.getElementById("table_body").innerHTML = "";
+  console.log(global)
+    global.map((item, index) => {
+        let newRow = `<tr>
+        <td>${item.name}</td>
+        <td>${item.employeeID}</td>
+        <td>${item.designation}</td>
+        <td>${item.city}</td>
+        <td>${item.contact}</td>
+        <td><button type="button">Edit</button>&nbsp<button onClick=deleteData(${index}) type="button">Delete</button></td></td>`
+        document.getElementById("table_body").innerHTML += newRow;
+
     })
-  })
+}
 
 
+function deleteData(index) {
+    global.splice(index, 1);
+    render();
+}
   
 
